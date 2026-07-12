0d046e27ec4363ae45a138b3e1a23cc389fda580
jasman046 <2300018046@webmail.uad.ac.id>
2026-07-12 16:50:53 +0700

    Paksa form login dan produk pakai HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa form Laravel pakai HTTPS kalau lagi di server Railway
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
